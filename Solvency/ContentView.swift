d087af4bb8daecb6e4af41e162421ed100cec85c
adamq1337 <adam.kajp@gmail.com>
2025-01-08 23:43:17 +0100

    Initial Commit

---
+++ Solvency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Solvency
+//
+//  Created by Adam Persson on 2025-01-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Solvency/SolvencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolvencyApp.swift
+//  Solvency
+//
+//  Created by Adam Persson on 2025-01-08.
+//
+
+import SwiftUI
+
+@main
+struct SolvencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
